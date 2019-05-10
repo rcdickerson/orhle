fa49dbed1c72de486b0342df9b6e69803c47d93a
Rob Dickerson <rc.dickerson@gmail.com>
2019-05-10 00:03:36 -0400

    Initial imp / standard hoare logic verifier

---
+++ Imp.hs
@@ -0,0 +1,71 @@
+module Imp where
+
+infix 1 :=
+infix 2 :=:
+infixl 6 :+:, :-:
+infixl 7 :*:
+
+type Var = String
+
+data AExp
+  = I Int
+  | V Var
+  | AExp :+: AExp
+  | AExp :-: AExp
+  | AExp :*: AExp
+  deriving (Show)
+
+data BExp
+  = BTrue
+  | BFalse
+  | AExp :=: AExp
+  | BNot BExp
+  | BAnd BExp BExp
+  deriving (Show)
+
+bor :: BExp -> BExp -> BExp
+bor bexp1 bexp2 = bsimpl $ BNot $ BAnd (BNot bexp1) (BNot bexp2)
+
+impl :: BExp -> BExp -> BExp
+impl bexp1 bexp2 = bsimpl $ bor (BNot bexp1) bexp2
+
+bsimpl :: BExp -> BExp
+bsimpl (BNot (BNot bexp)) = bexp
+bsimpl (BNot bexp) = BNot (bsimpl bexp)
+bsimpl (BAnd bexp1 bexp2) = BAnd (bsimpl bexp1) (bsimpl bexp2)
+bsimpl bexp = bexp
+
+data UFunc = UFunc {
+  fName :: String,
+  preCond :: BExp,
+  postCond :: BExp
+} deriving (Show)
+
+data Stmt
+  = Skip
+  | Var := AExp
+  | Seq [Stmt]
+  | If BExp Stmt Stmt
+  | Call UFunc
+  deriving (Show)
+
+type Prog = Stmt
+
+type State = [(Var, Int)]
+
+asubst :: AExp -> Var -> AExp -> AExp
+asubst aexp var repl =
+  case aexp of
+    I i -> I i
+    V v -> if (v == var) then repl else aexp
+    lhs :+: rhs -> (asubst lhs var repl) :+: (asubst rhs var repl)
+    lhs :-: rhs -> (asubst lhs var repl) :-: (asubst rhs var repl)
+    lhs :*: rhs -> (asubst lhs var repl) :*: (asubst rhs var repl)
+
+bsubst :: BExp -> Var -> AExp -> BExp
+bsubst bexp var aexp =
+  case bexp of
+    lhs :=: rhs -> (asubst lhs var aexp) :=: (asubst rhs var aexp)
+    BNot b -> BNot (bsubst b var aexp)
+    BAnd b1 b2 -> BAnd (bsubst b1 var aexp) (bsubst b2 var aexp)
+    _ -> bexp
---
+++ Verifier.hs
@@ -0,0 +1,64 @@
+module Verifier where
+
+import Imp
+import Z3.Monad
+
+type Cond = BExp
+
+condToZ3 :: Cond -> Z3 AST
+condToZ3 cond =
+  case cond of
+    BTrue -> mkTrue
+    BFalse -> mkFalse
+    aexp1 :=: aexp2 -> do
+      z3Exp1 <- aexpToZ3 aexp1
+      z3Exp2 <- aexpToZ3 aexp2
+      mkEq z3Exp1 z3Exp2
+    BNot bexp -> condToZ3 bexp >>= mkNot
+    BAnd bexp1 bexp2 -> do
+      z3Exp1 <- condToZ3 bexp1
+      z3Exp2 <- condToZ3 bexp2
+      mkAnd [z3Exp1, z3Exp2]
+
+aexpToZ3 :: AExp -> Z3 AST
+aexpToZ3 aexp =
+  case aexp of
+    I i -> mkIntNum i
+    V var -> mkStringSymbol var >>= mkIntVar
+    aexp1 :+: aexp2 -> aexpBinopToZ3 mkAdd aexp1 aexp2
+    aexp1 :-: aexp2 -> aexpBinopToZ3 mkSub aexp1 aexp2
+    aexp1 :*: aexp2 -> aexpBinopToZ3 mkMul aexp1 aexp2
+
+aexpBinopToZ3 :: ([AST] -> Z3 AST) -> AExp -> AExp -> Z3 AST
+aexpBinopToZ3 f aexp1 aexp2 = do
+  lhs <- aexpToZ3 aexp1
+  rhs <- aexpToZ3 aexp2
+  f [lhs, rhs]
+
+hleVcs :: Cond -> Prog -> Cond -> Cond
+hleVcs pre prog post = impl pre (hleWp prog post)
+
+hleWp :: Stmt -> Cond -> Cond
+hleWp stmt post =
+  case stmt of
+    Skip        -> post
+    Seq []      -> post
+    Seq (s:ss)  -> hleWp s (hleWp (Seq ss) post)
+    Call func   -> BAnd (impl (postCond func) post)
+                        (preCond func)
+    var := aexp -> bsubst post var aexp
+    If c s1 s2  -> BAnd (impl c (hleWp s1 post))
+                        (impl (BNot c) (hleWp s2 post))
+
+rhleVerify :: Cond -> Prog -> Prog -> Cond -> Cond
+rhleVerify pre uprog eprog post = BTrue
+
+
+-------------------------------------
+-- Useful for REPL experimentation --
+-------------------------------------
+func1 = UFunc "f1" ((V "x") :=: (I 0)) ((V "z") :=: (I 3))
+func2 = UFunc "f2" BTrue ((V "z") :=: ((V "x") :+: (I 3)))
+funcRand = UFunc "rand" BTrue BTrue
+prog1 = Seq ["x" := (I 0), "y" := (I 0), Call func2]
+cond1 = (V "z") :=: (I 3)
