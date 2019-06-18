f6ae6c0befe21e2fd8011ab8f0874b8d7424294e
Qianchuan Ye <yeqianchuan@gmail.com>
2019-06-18 18:04:36 -0400

    Initial setup for proofs.

---
+++ theories/Maps.v
@@ -0,0 +1,144 @@
+(** * Maps: Total and Partial Maps *)
+
+(* Adapted from the Software Foundations series:
+   https://softwarefoundations.cis.upenn.edu/ *)
+
+Require Import
+        Coq.Strings.String
+        Coq.Logic.FunctionalExtensionality.
+
+(* ################################################################# *)
+(** * Total Maps *)
+
+Definition total_map (A : Type) := string -> A.
+
+Definition t_empty {A : Type} (v : A) : total_map A :=
+  (fun _ => v).
+
+Definition t_update {A : Type} (m : total_map A)
+                    (x : string) (v : A) :=
+  fun x' => if string_dec x x' then v else m x'.
+
+Notation "'_' '!->' v" := (t_empty v)
+  (at level 100, right associativity).
+
+Notation "x '!->' v ';' m" := (t_update m x v)
+                              (at level 100, v at next level, right associativity).
+
+Lemma t_apply_empty : forall (A : Type) (x : string) (v : A),
+    (_ !-> v) x = v.
+Proof.
+  auto.
+Qed.
+
+Lemma t_update_eq : forall (A : Type) (m : total_map A) x v,
+    (x !-> v ; m) x = v.
+Proof.
+  intros. unfold t_update. now destruct string_dec.
+Qed.
+
+Theorem t_update_neq : forall (A : Type) (m : total_map A) x1 x2 v,
+    x1 <> x2 ->
+    (x1 !-> v ; m) x2 = m x2.
+Proof.
+  intros. unfold t_update. now destruct string_dec.
+Qed.
+
+Lemma t_update_shadow : forall (A : Type) (m : total_map A) x v1 v2,
+    (x !-> v2 ; x !-> v1 ; m) = (x !-> v2 ; m).
+Proof.
+  intros. extensionality x'.
+  unfold t_update. now destruct string_dec.
+Qed.
+
+Theorem t_update_same : forall (A : Type) (m : total_map A) x,
+    (x !-> m x ; m) = m.
+Proof.
+  intros. extensionality x'.
+  unfold t_update. destruct string_dec. congruence. easy.
+Qed.
+
+Theorem t_update_permute : forall (A : Type) (m : total_map A)
+                                  v1 v2 x1 x2,
+    x2 <> x1 ->
+    (x1 !-> v1 ; x2 !-> v2 ; m)
+    =
+    (x2 !-> v2 ; x1 !-> v1 ; m).
+Proof.
+  intros. extensionality x'.
+  unfold t_update. destruct string_dec; destruct string_dec; auto. congruence.
+Qed.
+
+Create HintDb maps discriminated.
+
+Hint Resolve
+     t_apply_empty t_update_eq t_update_neq
+     t_update_shadow t_update_same t_update_permute : maps.
+
+Hint Rewrite
+     t_apply_empty t_update_eq t_update_neq
+     t_update_shadow t_update_same : maps.
+
+(* ################################################################# *)
+(** * Partial maps *)
+
+Definition partial_map (A : Type) := total_map (option A).
+
+Definition empty {A : Type} : partial_map A :=
+  t_empty None.
+
+Definition update {A : Type} (m : partial_map A)
+           (x : string) (v : A) :=
+  (x !-> Some v ; m).
+
+Notation "x '|->' v ';' m" := (update m x v)
+  (at level 100, v at next level, right associativity).
+
+Notation "x '|->' v" := (update empty x v)
+  (at level 100).
+
+Lemma apply_empty : forall (A : Type) (x : string),
+    @empty A x = None.
+Proof.
+  intros. unfold empty. rewrite t_apply_empty.
+  reflexivity.
+Qed.
+
+Lemma update_eq : forall (A : Type) (m : partial_map A) x v,
+    (x |-> v ; m) x = Some v.
+Proof.
+  intros. unfold update. rewrite t_update_eq.
+  reflexivity.
+Qed.
+
+Theorem update_neq : forall (A : Type) (m : partial_map A) x1 x2 v,
+    x2 <> x1 ->
+    (x2 |-> v ; m) x1 = m x1.
+Proof.
+  intros A m x1 x2 v H.
+  unfold update. rewrite t_update_neq. reflexivity.
+  apply H. Qed.
+
+Lemma update_shadow : forall (A : Type) (m : partial_map A) x v1 v2,
+    (x |-> v2 ; x |-> v1 ; m) = (x |-> v2 ; m).
+Proof.
+  intros A m x v1 v2. unfold update. rewrite t_update_shadow.
+  reflexivity.
+Qed.
+
+Theorem update_same : forall (A : Type) (m : partial_map A) x v,
+    m x = Some v ->
+    (x |-> v ; m) = m.
+Proof.
+  intros A m x v H. unfold update. rewrite <- H.
+  apply t_update_same.
+Qed.
+
+Theorem update_permute : forall (A : Type) (m : partial_map A)
+                                x1 x2 v1 v2,
+    x2 <> x1 ->
+    (x1 |-> v1 ; x2 |-> v2 ; m) = (x2 |-> v2 ; x1 |-> v1 ; m).
+Proof.
+  intros A m x1 x2 v1 v2. unfold update.
+  apply t_update_permute.
+Qed.
