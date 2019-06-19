f37efcebb3274150bf86c7786ba752e93f9a3659
Qianchuan Ye <yeqianchuan@gmail.com>
2019-06-19 01:01:11 -0400

    Added Imp language syntax and semantics.

---
+++ theories/Imp.v
@@ -0,0 +1,162 @@
+(** * Imp: Simple Imperative Programs *)
+
+(* Adapted from the Software Foundations series:
+   https://softwarefoundations.cis.upenn.edu/ *)
+
+Require Import
+        Coq.Bool.Bool
+        Coq.Lists.List
+        Coq.Strings.String.
+
+Import Nat.
+Require Import Maps.
+
+(* ================================================================= *)
+(** ** States *)
+
+Definition state := total_map nat.
+
+(* ================================================================= *)
+(** ** Syntax  *)
+
+Inductive aexp : Type :=
+  | ANum (n : nat)
+  | AId (x : string)
+  | APlus (a1 a2 : aexp)
+  | AMinus (a1 a2 : aexp)
+  | AMult (a1 a2 : aexp).
+
+Inductive bexp : Type :=
+  | BTrue
+  | BFalse
+  | BEq (a1 a2 : aexp)
+  | BLe (a1 a2 : aexp)
+  | BNot (b : bexp)
+  | BAnd (b1 b2 : bexp).
+
+(* ================================================================= *)
+(** ** Notations *)
+
+Coercion AId : string >-> aexp.
+Coercion ANum : nat >-> aexp.
+
+Definition bool_to_bexp (b : bool) : bexp :=
+  if b then BTrue else BFalse.
+Coercion bool_to_bexp : bool >-> bexp.
+
+Bind Scope imp_scope with aexp.
+Bind Scope imp_scope with bexp.
+Delimit Scope imp_scope with imp.
+
+Notation "x + y" := (APlus x y) (at level 50, left associativity) : imp_scope.
+Notation "x - y" := (AMinus x y) (at level 50, left associativity) : imp_scope.
+Notation "x * y" := (AMult x y) (at level 40, left associativity) : imp_scope.
+Notation "x <= y" := (BLe x y) (at level 70, no associativity) : imp_scope.
+Notation "x = y" := (BEq x y) (at level 70, no associativity) : imp_scope.
+Notation "x && y" := (BAnd x y) (at level 40, left associativity) : imp_scope.
+Notation "'~' b" := (BNot b) (at level 75, right associativity) : imp_scope.
+
+(* ================================================================= *)
+(** ** Evaluation *)
+
+Fixpoint aeval (st : state) (a : aexp) : nat :=
+  match a with
+  | ANum n => n
+  | AId x => st x
+  | APlus a1 a2 => (aeval st a1) + (aeval st a2)
+  | AMinus a1 a2  => (aeval st a1) - (aeval st a2)
+  | AMult a1 a2 => (aeval st a1) * (aeval st a2)
+  end.
+
+Definition aseval (st : state) (al : list aexp) : list nat :=
+  map (aeval st) al.
+
+Fixpoint beval (st : state) (b : bexp) : bool :=
+  match b with
+  | BTrue       => true
+  | BFalse      => false
+  | BEq a1 a2   => (aeval st a1) =? (aeval st a2)
+  | BLe a1 a2   => (aeval st a1) <=? (aeval st a2)
+  | BNot b1     => negb (beval st b1)
+  | BAnd b1 b2  => andb (beval st b1) (beval st b2)
+  end.
+
+Definition empty_st := (_ !-> 0).
+
+Notation "a '!->' x" := (t_update empty_st a x) (at level 100).
+
+(* ################################################################# *)
+(** * Commands *)
+
+(* ================================================================= *)
+(** ** Syntax *)
+
+Inductive com : Type :=
+  | CSkip
+  | CAss (x : string) (a : aexp)
+  | CCall (x : string) (f : string) (args : list aexp)
+  | CSeq (c1 c2 : com)
+  | CIf (b : bexp) (c1 c2 : com)
+  | CWhile (b : bexp) (c : com).
+
+Bind Scope imp_scope with com.
+Notation "'SKIP'" :=
+   CSkip : imp_scope.
+Notation "x '::=' a" :=
+  (CAss x a) (at level 60) : imp_scope.
+Notation "x ':::=' f '$' a" :=
+  (CCall x f a) (at level 60) : imp_scope.
+Notation "c1 ;; c2" :=
+  (CSeq c1 c2) (at level 80, right associativity) : imp_scope.
+Notation "'WHILE' b 'DO' c 'END'" :=
+  (CWhile b c) (at level 80, right associativity) : imp_scope.
+Notation "'TEST' c1 'THEN' c2 'ELSE' c3 'FI'" :=
+  (CIf c1 c2 c3) (at level 80, right associativity) : imp_scope.
+
+(* ################################################################# *)
+(** * Evaluating Commands *)
+
+(* ----------------------------------------------------------------- *)
+(** *** Operational Semantics *)
+
+Structure funspec : Type :=
+  { pre : list nat -> Prop; post : nat -> list nat -> Prop }.
+
+Class Env : Type :=
+  { funsig : total_map funspec }.
+
+Reserved Notation "st '=[' c ']=>' st'"
+                  (at level 40).
+
+Inductive ceval {env : Env} : com -> state -> state -> Prop :=
+  | E_Skip : forall st,
+      st =[ SKIP ]=> st
+  | E_Ass  : forall st a1 n x,
+      aeval st a1 = n ->
+      st =[ x ::= a1 ]=> (x !-> n ; st)
+  | E_Spec : forall st args n x f,
+      (funsig f).(pre) (aseval st args) ->
+      (funsig f).(post) n (aseval st args) ->
+      st =[ x :::= f $ args ]=> (x !-> n ; st)
+  | E_Seq : forall c1 c2 st st' st'',
+      st  =[ c1 ]=> st'  ->
+      st' =[ c2 ]=> st'' ->
+      st  =[ c1 ;; c2 ]=> st''
+  | E_IfTrue : forall st st' b c1 c2,
+      beval st b = true ->
+      st =[ c1 ]=> st' ->
+      st =[ TEST b THEN c1 ELSE c2 FI ]=> st'
+  | E_IfFalse : forall st st' b c1 c2,
+      beval st b = false ->
+      st =[ c2 ]=> st' ->
+      st =[ TEST b THEN c1 ELSE c2 FI ]=> st'
+  | E_WhileFalse : forall b st c,
+      beval st b = false ->
+      st =[ WHILE b DO c END ]=> st
+  | E_WhileTrue : forall st st' st'' b c,
+      beval st b = true ->
+      st  =[ c ]=> st' ->
+      st' =[ WHILE b DO c END ]=> st'' ->
+      st  =[ WHILE b DO c END ]=> st''
+
+  where "st =[ c ]=> st'" := (ceval c st st').
