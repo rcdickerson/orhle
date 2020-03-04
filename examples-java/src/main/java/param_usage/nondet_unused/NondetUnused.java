59d37f74e6aea953caa17e3f527a3698b55bc618
Michael Zhang <zhan2793@purdue.edu>
2020-03-04 09:42:31 -0500

    Translate parameter usage examples

---
+++ examples-java/src/main/java/param_usage/nondet_unused/NondetUnused.java
@@ -0,0 +1,16 @@
+package param_usage.nondet_unused;
+
+import static utils.Utils.*;
+
+public class NondetUnused {
+    public static int run(int param1, int param2) {
+        int x = param1 + param2;
+        int ret;
+        if (randInt(1) == 0) {
+            ret = x - param2;
+        } else {
+            ret = param1;
+        }
+        return ret;
+    }
+}
