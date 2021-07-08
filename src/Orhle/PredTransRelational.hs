0ea5f0fec9847958adbb0b4358399ab7d03487c5
Rob Dickerson <rob@robd.io>
2021-07-08 15:16:31 -0400

    Removing single PTS (this is part of Ceili now)

--- src/Orhle/PredTransRelational.hs
+++ src/Orhle/PredTransRelational.hs
@@ -12,21 +12,8 @@ import           Orhle.PredTransTypes
 import           Orhle.Triple
 import           Orhle.VerifierEnv
 
--------------
--- AESpecs --
--------------
 
-data AESpecs = AESpecs
-  { aspecs :: SpecMap
-  , especs :: SpecMap
-  }
-
-instance CollectableNames AESpecs where
-  namesIn (AESpecs as es) = Set.union (allNames as) (allNames es)
-    where allNames = Map.foldr (Set.union . namesIn) Set.empty
-
-
-wpLoopFusion :: RevLoopFusion -> Assertion -> Verification Assertion
+wpLoopFusion :: LoopFusion -> Assertion -> Verification Assertion
 wpLoopFusion fusion post = do
   let abodies   = map rl_body $ rlf_aloops fusion
       ebodies   = map rl_body $ rlf_eloops fusion
@@ -46,7 +33,7 @@ wpLoopFusion fusion post = do
              _        -> return $ rlf_invar fusion
   freshVars     <- envFreshenAll vars
   let freshen    = Name.substituteAll vars freshVars
-  let qNames     = Name.toIntNames freshVars
+  let qNames     = map (\n -> TypedName n Int) freshVars
   let impPost    = freshen $ A.Imp (A.And $ invar:allNConds) post
   let decMeasure = case abodies of
                      [] -> let
@@ -60,37 +47,3 @@ wpLoopFusion fusion post = do
   inductive     <- (stepStrat bodyTrip) >>= (return . freshen . uncurry A.Imp)
   let sameIters = freshen $ A.Imp (A.And [invar, (A.Not $ A.And allConds)]) (A.And allNConds)
   return $ A.And [ invar, A.Forall qNames impPost, A.Forall qNames inductive, A.Forall qNames sameIters]
-
-spLoopFusion :: LoopFusion -> Assertion -> Verification Assertion
-spLoopFusion fusion pre = do
-  let abodies   = map l_body $ lf_aloops fusion
-      ebodies   = map l_body $ lf_eloops fusion
-      aconds    = map l_condAssertion $ lf_aloops fusion
-      econds    = map l_condAssertion $ lf_eloops fusion
-      anconds   = map A.Not aconds
-      enconds   = map A.Not econds
-      allBodies = abodies ++ ebodies
-      allConds  = aconds ++ econds
-      allNConds = anconds ++ enconds
-      varSet    = Set.unions $ map Name.namesIn allConds ++ map Name.namesIn allBodies
-      vars      = Set.toList varSet
-  invar <- case lf_invar fusion of
---             A.Hole _ -> do
---               firstPre <- envGetLoopSP
---               Inf.inferInvariants (BackwardHoudini _) (RhleTriple _)
---               let afilter assertion = checkValid False $ A.Imp firstPre assertion
---               invar <- findInvarWithFilter afilter allConds abodies ebodies post
---               case invar of
---                 Nothing     -> throwError "Unable to synthesize loop invariant"
---                 Just result -> return result
-             _  -> return $ lf_invar fusion
-  freshVars    <- envFreshenAll vars
-  let freshen   = Name.substituteAll vars freshVars
-  let qNames    = Name.toIntNames freshVars
-  let followsFromPre = freshen $ A.Imp pre invar
-  let bodyTrip  = RhleTriple (A.And $ invar:allConds) abodies ebodies invar
-  stepStrat    <- envForwardStepStrat
-  inductive    <- (stepStrat bodyTrip) >>= (return . freshen . uncurry A.Imp)
-  let sameIters = freshen $ A.Imp (A.And [invar, (A.Not $ A.And allConds)]) (A.And allNConds)
-  -- VCs: [A.Forall qNames inductive, A.Forall qNames sameIters A.Forall qNames impPost]
-  return $ A.And (invar:allNConds)
