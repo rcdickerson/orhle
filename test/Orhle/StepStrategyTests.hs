3812db0ce118990114d002357dfe7c59dbfc5842
Rob Dickerson <rob@robd.io>
2021-07-19 18:18:25 -0400

    Basic step strategy test.

--- test/Orhle/StepStrategyTests.hs
+++ test/Orhle/StepStrategyTests.hs
@@ -3,3 +3,15 @@
 module Orhle.StepStrategyTests(htf_thisModulesTests) where
 
 import Test.Framework
+
+import Ceili.CeiliEnv
+import Orhle.SpecImp
+import Orhle.StepStrategy
+
+emptyProg = impSeq [] :: SpecImpProgram
+
+test_backwardDisallowed = do
+  result <- runCeili defaultEnv $ backwardDisallowed [] []
+  case result of
+    Left  _ -> return () -- Pass
+    Right _ -> assertFailure "Unexpected success from backwardDisallowed strategy"
